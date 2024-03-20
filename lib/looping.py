92af73edfb775ac66b381aced54b5b4c16ecf8cc
Phil <philrsheridan@gmail.com>
2024-03-20 15:48:05 -0400

    passing all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for index in range(10):
+        print(10-index)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [(val * val) for val in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for index in range(1, 101):
+        if ((index % 3 == 0) and (index % 5 == 0)):
+            print("FizzBuzz")
+        elif (index % 3 == 0):
+            print("Fizz")
+        elif (index % 5 == 0):
+            print("Buzz")
+        else:
+            print(index)
